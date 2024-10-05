8d8e6955498a2d3fe4c478499ca0cafced5f40bf
KaguraiYoRoy <3363384229@qq.com>
2024-10-05 22:10:51 +0800

    Upload files

---
+++ .vitepress/config.mjs
@@ -0,0 +1,78 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  base: "docs.vertillusion.com",
+  head: [["link", { rel: "icon", href: "https://www.vertillusion.xyz/img/Newico.png" }]],
+  title: "Vertillusion Studio Docs",
+  description: "",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    // 页面导航
+    outlineTitle: "文章目录",
+    outline: [2, 6],
+
+    // 搜索框
+    search: {
+      provider: "local",
+      options: {
+        translations: {
+          button: {
+            buttonText: "搜索文档",
+            buttonAriaLabel: "搜索文档"
+          },
+          modal: {
+            noResultsText: "无法找到结果",
+            resetButtonTitle: "清除查询条件",
+            footer: {
+              selectLabel: "选择",
+              navigateUpKey: "↑",
+              navigateDownKey: "↓",
+              closeKey: "Esc",
+              submitKey: "Enter"
+            },
+          },
+        },
+      },
+    },
+    
+    // 页脚
+    footer: {
+      copyright: '© 版权所有 2019 - 2024, Vertillusion Studio - Meet, Inspire, Create..',
+      message: '利用 VitePress 构建'
+    },
+    
+    // 标题栏
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '项目', link: '/projects/LightFrame' },
+      { text: '关于我们', link: '/basic/about' }
+    ],
+
+    // 左侧菜单
+    sidebar: [
+      {
+        text: '基本信息',
+        items: [
+          { text: '工作室简介', link: '/basic/about' },
+          { text: '成员列表', link: '/basic/members' },
+          { text: '赞助者', link: 'https://lightframe.vertillusion.xyz' },
+          { text: '站点列表', link: '/basic/websites' }
+        ]
+      },
+      {
+        text: '项目',
+        items: [
+          { text: 'LightFrame', link: '/projects/LightFrame' },
+          { text: 'Minecraft服务器', link: '/projects/Minecraft' },
+          { text: 'Vertillusion Passport', link: '/projects/Passport' }
+        ]
+      },
+    ],
+
+    // 社交链接
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/Vertillusion/docs.vertillusion.com' }
+    ]
+  }
+})
---
+++ index.md
@@ -0,0 +1,27 @@
+---
+layout: home
+
+hero:
+  name: "Vertillusion Studio"
+  text: "Vertillusion 文档"
+  tagline: 
+#  image: // 启用后在首页显示一个图片。（如需启用请删除本条注释）
+#    src: // 请引用完整链接，比如 https://example.com/image.png （如需启用请删除本条注释）
+#    alt: image
+  actions:
+    - theme: brand
+      text: 项目
+      link: /projects/LightFrame
+    - theme: alt
+      text: 关于我们
+      link: /basic/about
+
+# features: // 启用后在首页显示四个小板块。（如需启用请删除本条注释）
+#  - title: Feature A // 标题
+#    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit // 描述
+#  - title: Feature B
+#    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+#  - title: Feature C
+#    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
